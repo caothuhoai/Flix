0b3fcc51b59f5b1131e9d33ca4235bc85d77254c
caothuhoai <caothuhoai00@gmail.com>
2022-02-18 23:45:26 -0500

    content update
    a new tab is added and grid view

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -20,14 +20,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
         // Do any additional setup after loading the view.
-        print("Hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
              // This will run when the network request returns
-             if let error = error {
-                print(error.localizedDescription)
+            if error != nil {
+
              } else if let data = data {
                  let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                     
@@ -35,7 +34,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 
                  self.tableView.reloadData()
                  
-                 print(dataDictionary)
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
                 // TODO: Reload your table view data
@@ -66,6 +64,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        print("Loading screen")
+        
+        //Task 1: Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        //Task 2: Passed the selected movie to the MovieDetails View Controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
 
 }
 
